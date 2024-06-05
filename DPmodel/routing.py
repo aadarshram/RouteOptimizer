922ad9b2f0d3df31695663f3a3bab4156d8c759b
Aadarsh R <ramachandranaadarsh@gmail.com>
2024-06-06 02:57:44 +0530

    Add interactive map showing route

--- DPmodel/routing.py
+++ DPmodel/routing.py
@@ -11,8 +11,10 @@ This real-life problem statement can be modelled as an extension to TSP.
 # Import libraries
 
 import requests
-import ortools
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+import folium
+from itertools import pairwise
+import webbrowser
 
 def create_distance_matrix(coordinates):
     '''
@@ -26,6 +28,7 @@ def create_distance_matrix(coordinates):
     if response.status_code != 200:
         raise Exception('Error in API request')
     data = response.json()
+    # or-tools requires integer entries in distance matrix. Since distance differences < 1m do not matter, we round of all distances in the matrix
     return [[round(element) for element in row] for row in data['distances']]
 
  
@@ -48,75 +51,11 @@ def create_data_model(coordinates, start_node, end_node):
 
     return data
 
-''' Update req: note: distance matrix accepts only integer values. Incorporate function to scale it by some 10^p acc to data and descale in results.'''
-
-def print_sol(manager, routing, solution):
+def solve_tsp(data):
     '''
-    Print solution details
+    Solve the route optimization problem
     '''
-    
-    # print solver status
-    i = routing.status()
-    if i == 0:
-        print('ROUTING NOT SOLVED')
-    elif i == 1:
-        print('ROUTING SUCCESS')
-    elif i == 2:
-        print('ROUTING PARTIAL SUCCESS. LOCAL OPTIMUM NOT REACHED')
-    elif i == 3:
-        print('ROUTING FAIL')
-    elif i == 4:
-        print('ROUTING FAIL TIMEOUT')
-    elif i == 5:
-        print('ROUTING INVALID')
-    elif i == 6:
-        print('ROUTING INFEASIBLE')
-    
-    print(f'Objective: {solution.ObjectiveValue()} m')
-    # # Print dropped nodes
-    # dropped_nodes = 'Dropped nodes:\n'
-    # for node in range(routing.Size()):
-    #     if routing.IsStart(node) or routing.IsEnd(node):
-    #         continue
-    #     if solution.Value(routing.NextVar(node)) == node: # if dropped,it points to itself
-    #         dropped_nodes += f'{manager.IndexToNode(node)}'
-    # print(dropped_nodes)
-        
-    index = routing.Start(0)
-    route_plan = 'Route:\n'
-    route_distance = 0
-    while not routing.IsEnd(index):
-        route_plan += f'{manager.IndexToNode(index)}->'
-
-        ''' Update req: This prints node indices. Change to print location addresses'''
-
-        prev_index = index
-        index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(prev_index, index, 0)
-    route_plan += f'{manager.IndexToNode(index)}\n'
-    print(route_plan)
-    print(f'Route distance: {route_distance/1000} km\n')
-    return None
-
-
-def main():
-
-    # User input
-    coordinates = [
-    (52.5200, 13.4050),  # Berlin
-    (53.5511, 9.9937),   # Hamburg
-    (48.1351, 11.5820),  # Munich
-    (50.9375, 6.9603),   # Cologne
-    (50.1109, 8.6821)    # Frankfurt
-    ] # (lat, lon) list of tuples of locations to visit
-
-    ''' Update: user input location to coordinates function'''
-    start_node = 0 # node index of the start point
-    end_node = 3 # node index of the end point
-    #penalty = 10 ** 10 # cost of skipping a node
-
-    data = create_data_model(coordinates, start_node, end_node) 
-
+    print('Solving.')
     # 'manager' is the index manager fior the routing model 'routing'.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot']) # args: (no. of nodes, no. of vehicles, start_end_index)
     # The manager takes care of all index <-> node conversions. We only need to deal with our nodes
@@ -158,8 +97,149 @@ def main():
     solution = routing.SolveWithParameters(search_parameters)
 
     if solution:
-        print_sol(manager, routing, solution)
+        index = routing.Start(0)
+        plan_output = []
+        while not routing.IsEnd(index):
+            plan_output.append(manager.IndexToNode(index))
+            index = solution.Value(routing.NextVar(index))
+        plan_output.append(manager.IndexToNode(index))
+    print('Done!')
+    return manager, routing, solution, plan_output
+    
 
+def print_sol(manager, routing, solution):
+    '''
+    Print solution details
+    '''
+    if solution:
+        
+        print(f'Objective: {solution.ObjectiveValue()} m')
+        # # Print dropped nodes
+        # dropped_nodes = 'Dropped nodes:\n'
+        # for node in range(routing.Size()):
+        #     if routing.IsStart(node) or routing.IsEnd(node):
+        #         continue
+        #     if solution.Value(routing.NextVar(node)) == node: # if dropped,it points to itself
+        #         dropped_nodes += f'{manager.IndexToNode(node)}'
+        # print(dropped_nodes)
+            
+        index = routing.Start(0)
+        route_plan = 'Route:\n'
+        route_distance = 0
+        while not routing.IsEnd(index):
+            route_plan += f'{manager.IndexToNode(index)}->'
+
+            ''' Update req: This prints node indices. Change to print location addresses'''
+
+            prev_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(prev_index, index, 0)
+        route_plan += f'{manager.IndexToNode(index)}\n'
+        print(route_plan)
+        print(f'Route distance: {route_distance/1000} km\n')
+    return None
+
+def display_on_map(coordinates):
+    ''' 
+    Function to view the optimized path on a map
+    '''
+    print('Creating map with locations.')
+    # Adding markers
+    
+    map = folium.Map()
+    folium.Marker(
+            location = list(coordinates[0]),
+            tooltip = 'Click here to view details of the place.',
+            popup = f'node: 0 ; location: {coordinates[0]}',
+            icon = folium.Icon(color = 'green', prefix = 'fa', icon = '0')
+        ).add_to(map)
+
+    for i, coord in enumerate(coordinates[1:-1]):
+        folium.Marker(
+            location = list(coord),
+            tooltip = 'Click here to view details of the place.',
+            popup = f'node: {i+1}; location: {coord}',
+            icon = folium.Icon(color = 'red', prefix = 'fa', icon = f'{i+1}')
+        ).add_to(map)
+    print('Finding optimized route.')
+    # request route
+    coordinates = ';'.join([f'{lon},{lat}' for lat, lon in coordinates])
+    osrm_url = f"http://router.project-osrm.org/route/v1/driving/{coordinates}?overview=full&alternatives=true&geometries=geojson"
+    response = requests.get(osrm_url)
+    data = response.json()
+
+    route = data['routes'][0]['geometry']
+    legs = data['routes'][0]['legs']
+    len_legs = len(legs)
+    for i, (start, end) in enumerate(pairwise(route['coordinates'])):
+        distance = legs[i % len_legs]['distance'] / 1000
+        locations = [(start[1], start[0]), (end[1], end[0])]
+        folium.PolyLine(locations = locations, tooltip = 'Click here to view details of the route.', popup = f'Distance: {distance:.3f} km').add_to(map)
+    # DIsplay map with appropriate zoom
+    bounds = [[coord[1], coord[0]] for coord in route['coordinates']]
+    map.fit_bounds(bounds)
+    print('Done!')
+    # Save and open map in browser
+    map.save('map_display.html')
+    webbrowser.open('map_display.html')
+    return None
+
+def main():
+
+    # User input
+    # coordinates = [
+    # (52.5200, 13.4050),  # Berlin
+    # (53.5511, 9.9937),   # Hamburg
+    # (48.1351, 11.5820),  # Munich
+    # (50.9375, 6.9603),   # Cologne
+    # (50.1109, 8.6821)    # Frankfurt
+    # ] # (lat, lon) list of tuples of locations to visit
+
+    coordinates = [
+    (52.5163, 13.3777),  # Brandenburg Gate
+    (52.5218, 13.4132),  # Alexanderplatz
+    (52.5250, 13.3685),  # Berlin Central Station
+    (52.5194, 13.4010),  # Berlin Cathedral
+    (52.5050, 13.4399)   # East Side Gallery
+    ]
+    ''' Update: user input location to coordinates function'''
+    start_node = 0 # node index of the start point
+    end_node = 3 # node index of the end point (wont work for now)
+    #penalty = 10 ** 10 # cost of skipping a node
+
+    # create the data model
+    data = create_data_model(coordinates, start_node, end_node) 
+    # solve the problem
+    manager, routing, solution, route_plan = solve_tsp(data)
+    # print solver status
+    i = routing.status()
+    if i == 0:
+        print('ROUTING NOT SOLVED')
+    elif i == 1:
+        print('ROUTING SUCCESS')
+    elif i == 2:
+        print('ROUTING PARTIAL SUCCESS. LOCAL OPTIMUM NOT REACHED')
+    elif i == 3:
+        print('ROUTING FAIL')
+    elif i == 4:
+        print('ROUTING FAIL TIMEOUT')
+    elif i == 5:
+        print('ROUTING INVALID')
+    elif i == 6:
+        print('ROUTING INFEASIBLE')
+
+    optimal_coords = [coordinates[i] for i in route_plan]
+    
+    
+    # print solution details (if needed)
+    p = input('Do you want to print solution details? (y/n)')
+    if p == 'y':
+        print_sol(manager, routing, solution)
+    
+    # display on map (if needed)
+    m = input('Do you want to view the route on a map? (y/n)')
+    if m == 'y':
+        display_on_map(optimal_coords)
 if __name__ == '__main__':
     main()
 
